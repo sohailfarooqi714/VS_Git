822e52cda0b734cbf5b1e667af73cf70c620205f
Sohail Ali Farooqi <sohailfarooqi714@gmail.com>
2019-11-05 23:33:31 +0500

    First Commit

---
+++ main.lua
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+--DIFFERENT
+
+--[[local dialogues = { "String1", "String2" } --Initilize table of array
+for i = 1, 10 do
+    dialogues[i] = " "
+    print ( dialogues [i] )
+end ]]
+
+--DIFFERENT
+
+-- Path for the file to read
+--[[local path = system.pathForFile("Dialogues.docx", system.DocumentsDirectory )
+ 
+-- Open the file handle
+local file, errorString = io.open( path, "r" )
+ 
+if not file then
+    -- Error occurred; output the cause
+    print( "File error: " .. errorString )
+else
+    -- Read data from file
+    local contents = file:read( "*a" )
+    -- Output the file contents
+    print( "Contents of " .. path .. "\n" .. contents )
+    -- Close the file handle
+    io.close( file )
+end
+ 
+file = nil]]
+
+--DIFFERENT
+
+
+local path = system.pathForFile("hello.txt", system.DocumentsDirectory )
+local fhd = io.open( path )
+  
+-- Determine if file exists
+if fhd then
+   print( "File exists" )
+   fhd:close()
+else
+    print( "File does not exist!" )
+end
+
+local path = system.pathForFile( nil, system.DocumentsDirectory )
+print( path )
